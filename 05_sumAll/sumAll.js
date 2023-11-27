6086b617cead0f291917ca825dc44bb849890c9b
Regan <reganpuah@gmail.com>
2023-11-28 07:14:34 +0800

    Exercise completed (function passes all tests)

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,8 @@
 const sumAll = function(a, b) {
+    if (typeof a != "number" || typeof b != "number"){
+        return 'ERROR'
+    }
+    
     if (a < 0 || b < 0) {
         return 'ERROR'
     }
