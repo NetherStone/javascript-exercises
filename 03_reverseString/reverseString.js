342b19c6576054d71e794b12d1e51ff76df6d0d9
Regan <reganpuah@gmail.com>
2023-11-27 11:28:29 +0800

    Completed the exercise (function passed all tests)

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    var splitString = string.split('');
+    splitString = splitString.reverse();
+    splitString = splitString.join('');
+    return splitString
 };
 
 // Do not edit below this line
