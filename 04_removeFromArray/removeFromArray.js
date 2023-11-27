f2c81ff98cbf0bf7d4e90bf8e05f58200736cb18
Regan <reganpuah@gmail.com>
2023-11-28 06:23:16 +0800

    Exercise complete (function passes all tests)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(array, ...toRemove) {
+    let editedArray = array;
+    const args = Array.from(toRemove);
+    for (let i = 0; i < args.length; i++) {
+        while (editedArray.includes(args[i])) {
+            let indexToRemove = editedArray.indexOf(args[i]);
+            editedArray.splice(indexToRemove, 1);
+        }
+    }
+    return editedArray
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
