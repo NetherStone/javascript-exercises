99965ce0017c5fcb42fc4631cc7511bcb09e4321
Regan <reganpuah@gmail.com>
2023-11-28 07:00:56 +0800

    Created function for exercise, first test pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    let finalSum = 0;
+    let smallNumber = 0;
+    let bigNumber = 0;
+    if (a < b) {
+        smallNumber = a;
+        bigNumber = b;
+    } else {
+        smallNumber = b;
+        bigNumber = a;
+    }
+    for (let i = smallNumber; i < bigNumber; i++) {
+        finalSum += i
+    }
+    finalSum += bigNumber
 
+    return finalSum
 };
 
 // Do not edit below this line
