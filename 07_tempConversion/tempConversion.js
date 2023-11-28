b70e25381f61abecfcc468b2e761b985564b50fc
Regan <reganpuah@gmail.com>
2023-11-28 10:55:25 +0800

    Exercise completed (Both functions passed all tests)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,11 +2,15 @@
 const convertToCelsius = function(fahrenheit) {
   let convertedCelsius = 0;
   convertedCelsius = (fahrenheit - 32) * (5/9); // Formula for °F to °C:  x °F ≘ (x − 32) × 5/9 °C 
-  convertedCelsius = parseFloat(convertedCelsius.toFixed(1))
-  return convertedCelsius
+  convertedCelsius = parseFloat(convertedCelsius.toFixed(1));
+  return convertedCelsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let convertedFahrenheit = 0;
+  convertedFahrenheit = (celsius * (9 / 5)) + 32; // Formula for °C to °F: x °C ≘ (x × 9/5 + 32) °F 
+  convertedFahrenheit = parseFloat(convertedFahrenheit.toFixed(1));
+  return convertedFahrenheit;
 };
 
 console.log(convertToCelsius(32))
