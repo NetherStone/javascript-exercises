90111529d7ee7d678f1ea05a90c72d1779334bcd
Regan <reganpuah@gmail.com>
2023-11-28 10:39:38 +0800

    Added conversion from °F to °C.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,4 +1,7 @@
-const convertToCelsius = function() {
+//Formulas for conversion taken from: https://en.wikipedia.org/wiki/Conversion_of_scales_of_temperature
+const convertToCelsius = function(fahrenheit) {
+  let convertedCelsius = 0
+  convertedCelsius = (fahrenheit - 32) * (5/9) // Formula for °F to °C:  x °F ≘ (x − 32) × 5/9 °C 
 };
 
 const convertToFahrenheit = function() {
