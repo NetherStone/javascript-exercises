174abeffed4b218bae64f8ab27a251cb248ded3e
Regan <reganpuah@gmail.com>
2023-11-28 10:50:21 +0800

    Rounded conversion to 1 decimal place

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
 //Formulas for conversion taken from: https://en.wikipedia.org/wiki/Conversion_of_scales_of_temperature
 const convertToCelsius = function(fahrenheit) {
-  let convertedCelsius = 0
-  convertedCelsius = (fahrenheit - 32) * (5/9) // Formula for °F to °C:  x °F ≘ (x − 32) × 5/9 °C 
+  let convertedCelsius = 0;
+  convertedCelsius = (fahrenheit - 32) * (5/9); // Formula for °F to °C:  x °F ≘ (x − 32) × 5/9 °C 
+  convertedCelsius = parseFloat(convertedCelsius.toFixed(1))
+  return convertedCelsius
 };
 
 const convertToFahrenheit = function() {
 };
+
+console.log(convertToCelsius(32))
 
 // Do not edit below this line
 module.exports = {
