c39400f852df6f3d201963f4140c8fe7549dd1f8
Regan <reganpuah@gmail.com>
2023-12-14 16:25:57 +0800

    Function cleared all tests. Did with for loop instead for array methods as I find a for loop is more readable than using reduce.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -3,8 +3,8 @@ const findTheOldest = function(people) {
     let oldestPerson = people[0];
 
     for (let i = 0; i < people.length; i++) {
-        const currentLifespan = people[i].yearOfDeath || currentDate.getFullYear() - people[i].yearOfBirth;
-        const maxLifespan = oldestPerson.yearOfDeath || currentDate.getFullYear() - oldestPerson.yearOfBirth;
+        const currentLifespan = (people[i].yearOfDeath || currentDate.getFullYear()) - people[i].yearOfBirth;
+        const maxLifespan = (oldestPerson.yearOfDeath || currentDate.getFullYear()) - oldestPerson.yearOfBirth;
 
         if (currentLifespan > maxLifespan) {
             oldestPerson = people[i];
