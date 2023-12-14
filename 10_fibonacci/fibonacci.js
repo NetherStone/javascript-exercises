9a8b2a216fbd6d131485beef0fa06933d4ab9834
Regan <reganpuah@gmail.com>
2023-12-14 10:59:28 +0800

    Added checker for inputs.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(inputNumber) {
+    if (typeof inputNumber === "string"){
+        inputNumber = parseInt(inputNumber);
+    }
 
+    if (inputNumber < 0){
+        return "OOPS"
+    }
 };
 
 // Do not edit below this line
