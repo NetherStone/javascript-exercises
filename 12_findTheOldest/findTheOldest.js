32add9deef8d61b227d75d251097a7f7f8beb21c
Regan <reganpuah@gmail.com>
2023-12-14 16:00:36 +0800

    Cleared first test...

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = people[0];
 
-};
+    for (let i = 0; i < people.length; i++) {
+        const currentLifespan = people[i].yearOfDeath - people[i].yearOfBirth;
+        const maxLifespan = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
 
+        if (currentLifespan > maxLifespan) {
+            oldestPerson = people[i];
+        }
+    }
+
+    return oldestPerson
+}
 // Do not edit below this line
 module.exports = findTheOldest;
