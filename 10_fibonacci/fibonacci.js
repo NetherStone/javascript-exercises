41c42c35ce3e0088ca0007805f8d8cce722a7ac4
Regan <reganpuah@gmail.com>
2023-12-14 11:43:48 +0800

    Created main part of function.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -4,8 +4,20 @@ const fibonacci = function(inputNumber) {
     }
 
     if (inputNumber < 0){
-        return "OOPS"
+        return "OOPS";
     }
+
+    let fibonacciArray = [];
+
+    for (let i = 1; i < inputNumber + 1; i++) {
+        if (i === 1 || i === 2) {
+            fibonacciArray.push(1);
+        } else {
+            fibonacciArray.push(fibonacciArray[i-3] + fibonacciArray[i-2]);
+        }
+    }
+
+    return fibonacciArray[inputNumber-1];
 };
 
 // Do not edit below this line
