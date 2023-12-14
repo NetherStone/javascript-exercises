ea9e21edb6da404e42eaee0253c76869382943ea
Regan <reganpuah@gmail.com>
2023-12-14 12:11:43 +0800

    Function passes the test.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const Titles = books.map(book => book.title);
+    return Titles
 };
 
 // Do not edit below this line
